f7c66d194fa2bf04e4f24f11f892a9c995abe774
atilla Arslan <toulouse663831@gmail.com>
2024-01-25 00:13:43 +0300

    personal web site

---
+++ web sitesi vize icin/script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById("downloadCvButton").addEventListener("click", function() {
+    // CV indirme bağlantısını tıklama olayını tetikle
+    document.getElementById("cvDownloadLink").click();
+});
